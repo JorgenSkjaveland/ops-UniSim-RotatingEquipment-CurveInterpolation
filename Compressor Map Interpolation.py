b849ad44e181433f5550b4334ab06d56d417debf
JorgenSkjaveland <jorgen.skjaveland@akerbp.com>
2026-03-12 14:44:28 +0100

    Added .gitignore file and updated Compressor Map Interpolation.py and README.md files.

--- Compressor Map Interpolation.py
+++ Compressor Map Interpolation.py
@@ -3,7 +3,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import scipy as sp
 from pathlib import Path
 import os
 
@@ -17,11 +16,11 @@ def EfficiencyCurve(dataframe: pd.DataFrame, name: str, n: int = 25):
     Flow = np.flip(dataframe.iloc[:, 0].to_numpy())
     Head = np.flip(dataframe.iloc[:, 1].to_numpy())
     Efficiency = np.flip(dataframe.iloc[:, 2].to_numpy())
-    spl_Head = sp.interpolate.CubicSpline(Flow, Head)
-    spl_Efficiency = sp.interpolate.CubicSpline(Flow, Efficiency)
+    polynomial_Head = np.polynomial.Polynomial.fit(Flow, Head, 3)
+    polynomial_Efficiency = np.polynomial.Polynomial.fit(Flow, Efficiency, 4)
     Flow_list = np.linspace(Flow.min(), Flow.max(), n)
-    Head_list = spl_Head(Flow_list)
-    Efficiency_list = spl_Efficiency(Flow_list)
+    Head_list = polynomial_Head(Flow_list)
+    Efficiency_list = polynomial_Efficiency(Flow_list)
     result_table = np.array([Flow_list,
                              Head_list,
                              Efficiency_list]).T
@@ -31,7 +30,7 @@ def EfficiencyCurve(dataframe: pd.DataFrame, name: str, n: int = 25):
                header=("Flow [m3/h] \t Head [kJ/kg] \t Efficiency [%]"))
     PlotEfficiencyFit(Flow, Efficiency, Flow_list, Efficiency_list, name, folder)
     PlotHeadFit(Flow, Head, Flow_list, Head_list, name, folder)
-    return Flow, Head, Efficiency, spl_Head(Flow_list), spl_Efficiency(Flow_list)
+    return Flow, Head, Efficiency, polynomial_Head(Flow_list), polynomial_Efficiency(Flow_list)
     
 def PlotEfficiencyFit(Flow, Efficiency, Flow_list, Efficiency_list, name=None, folder=None):
     ax = plt.subplot(111)
@@ -39,9 +38,9 @@ def PlotEfficiencyFit(Flow, Efficiency, Flow_list, Efficiency_list, name=None, f
     ax.plot(Flow_list, Efficiency_list, color="saddlebrown", label="Outputs")
     plt.legend()
     if name != None:
-        ax.set_title(f"Cubic Spline fit: {name}")
+        ax.set_title(f"Polynomial fit: {name}")
     else:
-        ax.set_title("Cubic Spline fit")
+        ax.set_title("Polynomial fit")
     ax.set_ylabel("Polytropic Efficiency [%]")
     ax.set_xlabel("Flow [m3/h]")
     plt.savefig(f"{folder}/Efficiency Curve - {name[0:-4]}.png")
@@ -54,18 +53,51 @@ def PlotHeadFit(Flow, Head, Flow_list, Head_list, name=None, folder=None):
     ax.plot(Flow_list, Head_list, color="steelblue", label="Outputs")
     plt.legend()
     if name != None:
-        ax.set_title(f"Cubic Spline fit: {name}")
+        ax.set_title(f"Polynomial fit: {name}")
     else:
-        ax.set_title("Cubic Spline fit")
+        ax.set_title("Polynomial fit")
     ax.set_ylabel("Head [kJ/kg]")
     ax.set_xlabel("Flow [m3/h]")
     plt.savefig(f"{folder}/Head Curve - {name[0:-4]}.png")
     plt.close()
+    return 0
+
+def Plot3D():
+    tsv_files = list((Path(__file__).parent / "CompressorDataForUniSim").glob("*.tsv"))
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.set_xlabel("Flow [m3/h]")
+    ax.set_ylabel("Head [kJ/kg]")
+    ax.set_zlabel("Poly. Eff [%]")
+    for file in tsv_files:
+        name = file.name
+        CompressorData = pd.read_csv(file, delimiter='\t')
+        ax.plot(CompressorData.values[:, 0], CompressorData.values[:, 1], CompressorData.values[:, 2], label=f"{name[7:-13]}")
+    plt.legend()
+    plt.show()
+    return 0
+
+def PlotFlowHead():
+    tsv_files = list((Path(__file__).parent / "CompressorDataForUniSim").glob("*.tsv"))
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Flow [m3/h]")
+    ax.set_ylabel("Head [kJ/kg]")
+    for file in tsv_files:
+        name = file.name
+        CompressorData = pd.read_csv(file, delimiter='\t')
+        ax.plot(CompressorData.values[:, 0], CompressorData.values[:, 1], label=f"{name[7:-15]}")
+    ax.set_title(f"{name[0:7]}")
+    fig.savefig(f"CompressorDataForUniSim/{name[0:7]}.png")
+    plt.legend()
+    plt.close()
+    return 0
     
 
 def GetDataForUniSim():
     dataframes, names = ReadCSVInFolder()
     for dataframe, name in zip(dataframes, names):
         EfficiencyCurve(dataframe, name)
+    PlotFlowHead()
+    Plot3D()
+    return 0
 
 GetDataForUniSim()
--- README.md
+++ README.md
@@ -15,6 +15,6 @@ $rpm = Revolutions per Minute$,
 
 <p>A compressor map like the one in <a href="#compressor-map">the figure above</a> contains Head, Volumetric flow and Efficiency in a single plot. This makes it more difficult to determine what the efficiency is for a given Head and Volumetric flow.</p>
 
-<p>The Python code takes in a csv-file with the discrete points (Head, Q), which each represents a known Efficiency. The program performes a Scipy Cubic spline, interpolation to determine efficiency between the known points. The program produces a tsv-file with n number (25 by standard) of evenly spread points with the columns <em>Flow [m3/h],  Head [kJ/kg],  Efficiency [%]<em> </p>
+<p>The Python code takes in a csv-file with the discrete points (Head, Q), which each represents a known Efficiency. The program performes a Numpy Polynomial interpolation to determine efficiency between the known points. The program produces a tsv-file with n number (25 by standard) of evenly spread points with the columns <em>Flow [m3/h],  Head [kJ/kg],  Efficiency [%]<em> </p>
 
 
